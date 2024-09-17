f4e59ea539688e5ec42316030f2a25d4fb5788ce
Aam <amadeusz.kryze@pm.me>
2024-09-17 10:17:02 +0200

    fix(bumper): apply potential final fix (#26)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced Docker image security by replacing `sudo` with the `tree`
    package and ensuring proper file ownership for `.mise.toml`.
    - Improved adaptability of the `bumper.sh` script to different execution
    environments, allowing for flexible sourcing of library files.
    
    - **Bug Fixes**
    - Simplified debugging logic in the `debug.sh` script, focusing on a
    more streamlined approach to debugging output.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- lib/debug.sh
+++ lib/debug.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
 init_debug() {
-  # -n; True if the length of string is non-zero.
-  if [[ (-n "${INETUM_POLAND_ACTION_BUMPER_TRACE:-}" && "${INETUM_POLAND_ACTION_BUMPER_TRACE}" == "true") && "${SHELLSPEC:-}" != "true" ]]; then
-    # KCOV_EXCL_START
-    set -x
-    export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
-    # KCOV_EXCL_STOP
-  fi
-
   if [[ -n "${DEBUG_GITHUB_EVENT_PATH:-}" ]]; then
     # shellcheck disable=SC2034
     GITHUB_EVENT_PATH="${DEBUG_GITHUB_EVENT_PATH}/data.json"
